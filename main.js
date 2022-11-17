249cec964ee7295b421d2258db434a8ab950a6ef
Humbert Evan <evan.humbert@outlook.com>
2022-11-17 23:34:07 +0100

    update

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const sectionBtn = document.getElementById("new-section-btn");
 const main = document.getElementById("main");
+const sectionModel = document.getElementById("section-model");
+const btn = document.getElementById("btn");
+const counterShow = document.getElementById("counter");
+let counter = 1
+
+counterShow.innerHTML = counter;
 
 sectionBtn.addEventListener("click", () => {
     const section = document.createElement("section");
-    document.body.appendChild(section);
+    section.innerHTML = sectionModel.innerHTML;
+    section.setAttribute('id', counter);
+    main.appendChild(section);
+    btn.href = "#"+counter;
+    counterShow.innerHTML = counter;
+    counter ++;
 });
+
